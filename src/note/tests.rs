232c956527c3bd513b90c5cea5f1330d7f5440b6
Victor Lopez <victor@dusk.network>
2019-12-14 17:37:42 +0100

    Test should grant obfuscated note will not save any inner state outside ser/de routine

--- src/note/tests.rs
+++ src/note/tests.rs
@@ -22,12 +22,13 @@ fn obfuscated_note() {
     let value = 25;
 
     let note = ObfuscatedNote::output(&pk, value);
-    let note = bincode::serialize(&note).unwrap();
-    let note: ObfuscatedNote = bincode::deserialize(note.as_slice()).unwrap();
 
     assert_eq!(note.utxo(), NoteUtxoType::Output);
     assert_eq!(note.note(), NoteType::Obfuscated);
 
     let proof = note.prove_value(&sk).unwrap();
+
+    let note = bincode::serialize(&note).unwrap();
+    let note: ObfuscatedNote = bincode::deserialize(note.as_slice()).unwrap();
     note.verify_value(&proof).unwrap();
 }
