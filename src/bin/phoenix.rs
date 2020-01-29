f8f0742a20c32c851bbe66452288c9ee929166e5
Victor Lopez <victor@dusk.network>
2020-01-29 16:31:32 +0100

    Phoenix binary

--- src/bin/phoenix.rs
+++ src/bin/phoenix.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello world!");
+use phoenix::{rpc, Db};
+use tonic::transport::Server;
+use tracing::info;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = "[::1]:50051".parse().unwrap();
+
+    let db = Db::new().unwrap();
+    let phoenix = rpc::Server::new(db);
+
+    let subscriber = tracing_subscriber::fmt::Subscriber::builder()
+        .with_max_level(tracing::Level::TRACE)
+        .finish();
+    tracing::subscriber::set_global_default(subscriber)?;
+
+    info!("Listening on {}...", addr);
+
+    // TODO - Check why tonic isnt submitting logs to tracing subscribed handler
+    Server::builder()
+        .add_service(rpc::phoenix_server::PhoenixServer::new(phoenix))
+        .serve(addr)
+        .await?;
+
+    Ok(())
 }
