5e0d720245c1ad367ce16c230baf31c19f27072a
emanuele francioni <emanuele@dusk.network>
2020-03-25 13:20:30 +0100

    add error structure

--- autogen/go/monitor/monitor.pb.go
+++ autogen/go/monitor/monitor.pb.go
@@ -24,6 +24,37 @@ var _ = math.Inf
 // proto package needs to be updated.
 const _ = proto.ProtoPackageIsVersion3 // please upgrade the proto package
 
+type Level int32
+
+const (
+	Level_WARN  Level = 0
+	Level_ERROR Level = 1
+	Level_FATAL Level = 2
+	Level_PANIC Level = 3
+)
+
+var Level_name = map[int32]string{
+	0: "WARN",
+	1: "ERROR",
+	2: "FATAL",
+	3: "PANIC",
+}
+
+var Level_value = map[string]int32{
+	"WARN":  0,
+	"ERROR": 1,
+	"FATAL": 2,
+	"PANIC": 3,
+}
+
+func (x Level) String() string {
+	return proto.EnumName(Level_name, int32(x))
+}
+
+func (Level) EnumDescriptor() ([]byte, []int) {
+	return fileDescriptor_44174b7b2a306b71, []int{0}
+}
+
 type SemverRequest struct {
 	Major                uint32   `protobuf:"varint,1,opt,name=major,proto3" json:"major,omitempty"`
 	Minor                uint32   `protobuf:"varint,2,opt,name=minor,proto3" json:"minor,omitempty"`
@@ -79,6 +110,179 @@ func (m *SemverRequest) GetPatch() uint32 {
 	return 0
 }
 
+type SlowdownAlert struct {
+	TimeSinceLastBlock   uint32   `protobuf:"varint,1,opt,name=timeSinceLastBlock,proto3" json:"timeSinceLastBlock,omitempty"`
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
+}
+
+func (m *SlowdownAlert) Reset()         { *m = SlowdownAlert{} }
+func (m *SlowdownAlert) String() string { return proto.CompactTextString(m) }
+func (*SlowdownAlert) ProtoMessage()    {}
+func (*SlowdownAlert) Descriptor() ([]byte, []int) {
+	return fileDescriptor_44174b7b2a306b71, []int{1}
+}
+
+func (m *SlowdownAlert) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_SlowdownAlert.Unmarshal(m, b)
+}
+func (m *SlowdownAlert) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_SlowdownAlert.Marshal(b, m, deterministic)
+}
+func (m *SlowdownAlert) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_SlowdownAlert.Merge(m, src)
+}
+func (m *SlowdownAlert) XXX_Size() int {
+	return xxx_messageInfo_SlowdownAlert.Size(m)
+}
+func (m *SlowdownAlert) XXX_DiscardUnknown() {
+	xxx_messageInfo_SlowdownAlert.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_SlowdownAlert proto.InternalMessageInfo
+
+func (m *SlowdownAlert) GetTimeSinceLastBlock() uint32 {
+	if m != nil {
+		return m.TimeSinceLastBlock
+	}
+	return 0
+}
+
+type Field struct {
+	Field                string   `protobuf:"bytes,1,opt,name=field,proto3" json:"field,omitempty"`
+	Value                string   `protobuf:"bytes,2,opt,name=value,proto3" json:"value,omitempty"`
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
+}
+
+func (m *Field) Reset()         { *m = Field{} }
+func (m *Field) String() string { return proto.CompactTextString(m) }
+func (*Field) ProtoMessage()    {}
+func (*Field) Descriptor() ([]byte, []int) {
+	return fileDescriptor_44174b7b2a306b71, []int{2}
+}
+
+func (m *Field) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_Field.Unmarshal(m, b)
+}
+func (m *Field) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_Field.Marshal(b, m, deterministic)
+}
+func (m *Field) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_Field.Merge(m, src)
+}
+func (m *Field) XXX_Size() int {
+	return xxx_messageInfo_Field.Size(m)
+}
+func (m *Field) XXX_DiscardUnknown() {
+	xxx_messageInfo_Field.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_Field proto.InternalMessageInfo
+
+func (m *Field) GetField() string {
+	if m != nil {
+		return m.Field
+	}
+	return ""
+}
+
+func (m *Field) GetValue() string {
+	if m != nil {
+		return m.Value
+	}
+	return ""
+}
+
+type ErrorAlert struct {
+	Level                Level    `protobuf:"varint,1,opt,name=level,proto3,enum=monitor.Level" json:"level,omitempty"`
+	Msg                  string   `protobuf:"bytes,2,opt,name=msg,proto3" json:"msg,omitempty"`
+	Timestamp            int64    `protobuf:"varint,3,opt,name=timestamp,proto3" json:"timestamp,omitempty"`
+	File                 string   `protobuf:"bytes,4,opt,name=file,proto3" json:"file,omitempty"`
+	Line                 uint32   `protobuf:"varint,5,opt,name=line,proto3" json:"line,omitempty"`
+	Function             string   `protobuf:"bytes,6,opt,name=function,proto3" json:"function,omitempty"`
+	Fields               []*Field `protobuf:"bytes,7,rep,name=fields,proto3" json:"fields,omitempty"`
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
+}
+
+func (m *ErrorAlert) Reset()         { *m = ErrorAlert{} }
+func (m *ErrorAlert) String() string { return proto.CompactTextString(m) }
+func (*ErrorAlert) ProtoMessage()    {}
+func (*ErrorAlert) Descriptor() ([]byte, []int) {
+	return fileDescriptor_44174b7b2a306b71, []int{3}
+}
+
+func (m *ErrorAlert) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_ErrorAlert.Unmarshal(m, b)
+}
+func (m *ErrorAlert) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_ErrorAlert.Marshal(b, m, deterministic)
+}
+func (m *ErrorAlert) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_ErrorAlert.Merge(m, src)
+}
+func (m *ErrorAlert) XXX_Size() int {
+	return xxx_messageInfo_ErrorAlert.Size(m)
+}
+func (m *ErrorAlert) XXX_DiscardUnknown() {
+	xxx_messageInfo_ErrorAlert.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_ErrorAlert proto.InternalMessageInfo
+
+func (m *ErrorAlert) GetLevel() Level {
+	if m != nil {
+		return m.Level
+	}
+	return Level_WARN
+}
+
+func (m *ErrorAlert) GetMsg() string {
+	if m != nil {
+		return m.Msg
+	}
+	return ""
+}
+
+func (m *ErrorAlert) GetTimestamp() int64 {
+	if m != nil {
+		return m.Timestamp
+	}
+	return 0
+}
+
+func (m *ErrorAlert) GetFile() string {
+	if m != nil {
+		return m.File
+	}
+	return ""
+}
+
+func (m *ErrorAlert) GetLine() uint32 {
+	if m != nil {
+		return m.Line
+	}
+	return 0
+}
+
+func (m *ErrorAlert) GetFunction() string {
+	if m != nil {
+		return m.Function
+	}
+	return ""
+}
+
+func (m *ErrorAlert) GetFields() []*Field {
+	if m != nil {
+		return m.Fields
+	}
+	return nil
+}
+
 type EmptyRequest struct {
 	XXX_NoUnkeyedLiteral struct{} `json:"-"`
 	XXX_unrecognized     []byte   `json:"-"`
@@ -89,7 +293,7 @@ func (m *EmptyRequest) Reset()         { *m = EmptyRequest{} }
 func (m *EmptyRequest) String() string { return proto.CompactTextString(m) }
 func (*EmptyRequest) ProtoMessage()    {}
 func (*EmptyRequest) Descriptor() ([]byte, []int) {
-	return fileDescriptor_44174b7b2a306b71, []int{1}
+	return fileDescriptor_44174b7b2a306b71, []int{4}
 }
 
 func (m *EmptyRequest) XXX_Unmarshal(b []byte) error {
@@ -120,7 +324,7 @@ func (m *EmptyResponse) Reset()         { *m = EmptyResponse{} }
 func (m *EmptyResponse) String() string { return proto.CompactTextString(m) }
 func (*EmptyResponse) ProtoMessage()    {}
 func (*EmptyResponse) Descriptor() ([]byte, []int) {
-	return fileDescriptor_44174b7b2a306b71, []int{2}
+	return fileDescriptor_44174b7b2a306b71, []int{5}
 }
 
 func (m *EmptyResponse) XXX_Unmarshal(b []byte) error {
@@ -156,7 +360,7 @@ func (m *BlockUpdate) Reset()         { *m = BlockUpdate{} }
 func (m *BlockUpdate) String() string { return proto.CompactTextString(m) }
 func (*BlockUpdate) ProtoMessage()    {}
 func (*BlockUpdate) Descriptor() ([]byte, []int) {
-	return fileDescriptor_44174b7b2a306b71, []int{3}
+	return fileDescriptor_44174b7b2a306b71, []int{6}
 }
 
 func (m *BlockUpdate) XXX_Unmarshal(b []byte) error {
@@ -213,7 +417,11 @@ func (m *BlockUpdate) GetBlockTimeSec() uint32 {
 }
 
 func init() {
+	proto.RegisterEnum("monitor.Level", Level_name, Level_value)
 	proto.RegisterType((*SemverRequest)(nil), "monitor.SemverRequest")
+	proto.RegisterType((*SlowdownAlert)(nil), "monitor.SlowdownAlert")
+	proto.RegisterType((*Field)(nil), "monitor.Field")
+	proto.RegisterType((*ErrorAlert)(nil), "monitor.ErrorAlert")
 	proto.RegisterType((*EmptyRequest)(nil), "monitor.EmptyRequest")
 	proto.RegisterType((*EmptyResponse)(nil), "monitor.EmptyResponse")
 	proto.RegisterType((*BlockUpdate)(nil), "monitor.BlockUpdate")
@@ -224,26 +432,39 @@ func init() {
 }
 
 var fileDescriptor_44174b7b2a306b71 = []byte{
-	// 290 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x84, 0x91, 0xc1, 0x4e, 0xb3, 0x40,
-	0x10, 0xc7, 0x3f, 0xbe, 0x96, 0x56, 0xa7, 0xa0, 0xc9, 0xa4, 0x36, 0xa4, 0xf1, 0x60, 0x38, 0x79,
-	0xea, 0x41, 0x13, 0x13, 0x0f, 0x1e, 0x6c, 0x62, 0xe2, 0x45, 0x13, 0xa9, 0x3e, 0x00, 0xc5, 0x51,
-	0x56, 0x59, 0x66, 0x85, 0xa9, 0x91, 0x27, 0xf1, 0x61, 0x7c, 0x39, 0xc3, 0x82, 0xad, 0x98, 0x18,
-	0x6f, 0xfc, 0x7e, 0xcc, 0xf0, 0x67, 0xff, 0x0b, 0xbe, 0xe6, 0x5c, 0x09, 0x17, 0x33, 0x53, 0xb0,
-	0x30, 0x0e, 0x5b, 0x0c, 0x6f, 0xc0, 0x5f, 0x90, 0x7e, 0xa5, 0x22, 0xa2, 0x97, 0x15, 0x95, 0x82,
-	0x63, 0x70, 0x75, 0xfc, 0xc4, 0x45, 0xe0, 0x1c, 0x38, 0x87, 0x7e, 0xd4, 0x80, 0xb5, 0x2a, 0xe7,
-	0x22, 0xf8, 0xdf, 0xda, 0x1a, 0x6a, 0x6b, 0x62, 0x49, 0xd2, 0xa0, 0xd7, 0x58, 0x0b, 0xe1, 0x0e,
-	0x78, 0x17, 0xda, 0x48, 0xd5, 0x7e, 0x31, 0xdc, 0x05, 0xbf, 0xe5, 0xd2, 0x70, 0x5e, 0x52, 0xf8,
-	0xee, 0xc0, 0x68, 0x9e, 0x71, 0xf2, 0x7c, 0x67, 0xee, 0x63, 0x21, 0x9c, 0xc0, 0x20, 0x25, 0xf5,
-	0x98, 0x8a, 0xcd, 0xec, 0x47, 0x2d, 0x21, 0x42, 0x3f, 0x8d, 0xcb, 0xd4, 0x66, 0x7a, 0x91, 0x7d,
-	0xc6, 0x7d, 0xd8, 0x16, 0xa5, 0xa9, 0x94, 0x58, 0x1b, 0x1b, 0xdb, 0x8b, 0x36, 0x02, 0xa7, 0xb0,
-	0x25, 0x6f, 0xe7, 0x9a, 0x57, 0xb9, 0x04, 0x7d, 0xfb, 0x4f, 0x6b, 0xc6, 0x10, 0xbc, 0x65, 0x1d,
-	0x7a, 0xab, 0x34, 0x2d, 0x28, 0x09, 0x5c, 0xfb, 0xbe, 0xe3, 0x8e, 0x3e, 0x1c, 0x18, 0x5e, 0x35,
-	0xcd, 0xe0, 0x29, 0xb8, 0x97, 0x94, 0x65, 0x8c, 0x93, 0xd9, 0x57, 0x77, 0x9d, 0xa6, 0xa6, 0x1b,
-	0xdf, 0x3d, 0xde, 0x3f, 0x3c, 0x81, 0xde, 0xbc, 0x22, 0xdc, 0xfb, 0x39, 0xf0, 0xd7, 0xde, 0x19,
-	0x8c, 0xae, 0x59, 0xd4, 0x43, 0x65, 0xdb, 0xc1, 0xf1, 0x7a, 0xf0, 0x5b, 0x5b, 0xbf, 0xaf, 0x2f,
-	0x07, 0xf6, 0x6e, 0x8f, 0x3f, 0x03, 0x00, 0x00, 0xff, 0xff, 0xe8, 0xec, 0x2f, 0x89, 0xec, 0x01,
-	0x00, 0x00,
+	// 505 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x84, 0x53, 0xcd, 0x6e, 0xd3, 0x40,
+	0x10, 0x6e, 0xea, 0x38, 0x3f, 0x93, 0x38, 0x44, 0x43, 0xa9, 0xac, 0x88, 0x43, 0x65, 0x21, 0x54,
+	0x71, 0xc8, 0x21, 0x95, 0x90, 0x90, 0x40, 0xc8, 0x41, 0xa9, 0x40, 0x0a, 0x01, 0x36, 0x45, 0x9c,
+	0x5d, 0x67, 0x53, 0x2f, 0xac, 0xbd, 0xc6, 0xde, 0xa4, 0xe4, 0x49, 0x78, 0x1a, 0x5e, 0x81, 0x67,
+	0x42, 0x3b, 0x76, 0x9c, 0x04, 0x41, 0x7b, 0x9b, 0xef, 0xdb, 0x99, 0xd9, 0xf9, 0xbe, 0x9d, 0x05,
+	0x27, 0x56, 0x89, 0xd0, 0x2a, 0x1b, 0xa6, 0x99, 0xd2, 0x0a, 0x9b, 0x25, 0xf4, 0x3e, 0x81, 0x33,
+	0xe7, 0xf1, 0x9a, 0x67, 0x8c, 0x7f, 0x5f, 0xf1, 0x5c, 0xe3, 0x09, 0xd8, 0x71, 0xf0, 0x55, 0x65,
+	0x6e, 0xed, 0xac, 0x76, 0xee, 0xb0, 0x02, 0x10, 0x2b, 0x12, 0x95, 0xb9, 0xc7, 0x25, 0x6b, 0x80,
+	0x61, 0xd3, 0x40, 0x87, 0x91, 0x6b, 0x15, 0x2c, 0x01, 0xef, 0x35, 0x38, 0x73, 0xa9, 0x6e, 0x17,
+	0xea, 0x36, 0xf1, 0x25, 0xcf, 0x34, 0x0e, 0x01, 0xb5, 0x88, 0xf9, 0x5c, 0x24, 0x21, 0x9f, 0x06,
+	0xb9, 0x1e, 0x4b, 0x15, 0x7e, 0x2b, 0xfb, 0xff, 0xe3, 0xc4, 0xbb, 0x00, 0xfb, 0x52, 0x70, 0xb9,
+	0x30, 0xfd, 0x97, 0x26, 0xa0, 0xdc, 0x36, 0x2b, 0x80, 0x61, 0xd7, 0x81, 0x5c, 0x71, 0x9a, 0xa5,
+	0xcd, 0x0a, 0xe0, 0xfd, 0xae, 0x01, 0x4c, 0xb2, 0x4c, 0x65, 0xc5, 0x9d, 0x4f, 0xc0, 0x96, 0x7c,
+	0xcd, 0x25, 0x95, 0xf6, 0x46, 0xbd, 0xe1, 0x56, 0xff, 0xd4, 0xb0, 0xac, 0x38, 0xc4, 0x3e, 0x58,
+	0x71, 0x7e, 0x53, 0x36, 0x32, 0x21, 0x3e, 0x86, 0xb6, 0x99, 0x28, 0xd7, 0x41, 0x9c, 0x92, 0x2c,
+	0x8b, 0xed, 0x08, 0x44, 0xa8, 0x2f, 0x85, 0xe4, 0x6e, 0x9d, 0x0a, 0x28, 0x36, 0x9c, 0x14, 0x09,
+	0x77, 0x6d, 0xd2, 0x43, 0x31, 0x0e, 0xa0, 0xb5, 0x5c, 0x25, 0xa1, 0x16, 0x2a, 0x71, 0x1b, 0x94,
+	0x5b, 0x61, 0x7c, 0x0a, 0x0d, 0xd2, 0x91, 0xbb, 0xcd, 0x33, 0xeb, 0xbc, 0xb3, 0x37, 0x1a, 0x89,
+	0x66, 0xe5, 0xa9, 0xd7, 0x83, 0xee, 0x24, 0x4e, 0xf5, 0xa6, 0x7c, 0x18, 0xef, 0x01, 0x38, 0x25,
+	0xce, 0x53, 0x95, 0xe4, 0xdc, 0xfb, 0x59, 0x83, 0x0e, 0x19, 0xf6, 0x39, 0x5d, 0x04, 0x9a, 0xe3,
+	0x29, 0x34, 0x22, 0x2e, 0x6e, 0x22, 0x4d, 0x9a, 0xeb, 0xac, 0x44, 0x66, 0xc0, 0x28, 0xc8, 0x23,
+	0x52, 0xd9, 0x65, 0x14, 0xdf, 0x23, 0x73, 0x00, 0x2d, 0xfd, 0xc3, 0x8f, 0xd5, 0x2a, 0xd1, 0x24,
+	0xd5, 0x61, 0x15, 0x46, 0x0f, 0xba, 0xd7, 0xe6, 0xd2, 0x2b, 0xf3, 0x6e, 0x3c, 0x2c, 0x65, 0x1f,
+	0x70, 0xcf, 0x46, 0x60, 0x93, 0xcd, 0xd8, 0x82, 0xfa, 0x17, 0x9f, 0xcd, 0xfa, 0x47, 0xd8, 0x06,
+	0x7b, 0xc2, 0xd8, 0x07, 0xd6, 0xaf, 0x99, 0xf0, 0xd2, 0xbf, 0xf2, 0xa7, 0xfd, 0x63, 0x13, 0x7e,
+	0xf4, 0x67, 0xef, 0xde, 0xf4, 0xad, 0xd1, 0xaf, 0x63, 0x68, 0xbe, 0x2f, 0x8c, 0xc0, 0x17, 0x60,
+	0xbf, 0xe5, 0x52, 0x2a, 0x3c, 0xad, 0xbc, 0x39, 0x58, 0xd2, 0xc1, 0x8e, 0x3f, 0xb4, 0xe4, 0x08,
+	0x9f, 0x83, 0x35, 0xde, 0x70, 0x7c, 0xf4, 0x77, 0xc2, 0x7d, 0x75, 0xaf, 0xa0, 0x33, 0x53, 0x5a,
+	0x2c, 0x37, 0xe4, 0x28, 0x9e, 0x54, 0x89, 0x7b, 0x0e, 0xdf, 0x51, 0x3e, 0x86, 0x5e, 0x51, 0xbe,
+	0xdd, 0xfc, 0xfd, 0xd1, 0xf7, 0x3f, 0xc3, 0x1d, 0x3d, 0x5e, 0x6e, 0x47, 0xa0, 0x35, 0xc6, 0x87,
+	0xbb, 0xc4, 0x6a, 0xad, 0xff, 0x5f, 0x7d, 0xdd, 0xa0, 0x8f, 0x7d, 0xf1, 0x27, 0x00, 0x00, 0xff,
+	0xff, 0x48, 0xe6, 0x5c, 0x05, 0xe9, 0x03, 0x00, 0x00,
 }
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -261,6 +482,8 @@ type MonitorClient interface {
 	Hello(ctx context.Context, in *SemverRequest, opts ...grpc.CallOption) (*EmptyResponse, error)
 	Bye(ctx context.Context, in *EmptyRequest, opts ...grpc.CallOption) (*EmptyResponse, error)
 	NotifyBlock(ctx context.Context, in *BlockUpdate, opts ...grpc.CallOption) (*EmptyResponse, error)
+	NotifySlowdown(ctx context.Context, in *SlowdownAlert, opts ...grpc.CallOption) (*EmptyResponse, error)
+	NotifyError(ctx context.Context, in *ErrorAlert, opts ...grpc.CallOption) (*EmptyResponse, error)
 }
 
 type monitorClient struct {
@@ -298,11 +521,31 @@ func (c *monitorClient) NotifyBlock(ctx context.Context, in *BlockUpdate, opts .
 	return out, nil
 }
 
+func (c *monitorClient) NotifySlowdown(ctx context.Context, in *SlowdownAlert, opts ...grpc.CallOption) (*EmptyResponse, error) {
+	out := new(EmptyResponse)
+	err := c.cc.Invoke(ctx, "/monitor.Monitor/NotifySlowdown", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *monitorClient) NotifyError(ctx context.Context, in *ErrorAlert, opts ...grpc.CallOption) (*EmptyResponse, error) {
+	out := new(EmptyResponse)
+	err := c.cc.Invoke(ctx, "/monitor.Monitor/NotifyError", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // MonitorServer is the server API for Monitor service.
 type MonitorServer interface {
 	Hello(context.Context, *SemverRequest) (*EmptyResponse, error)
 	Bye(context.Context, *EmptyRequest) (*EmptyResponse, error)
 	NotifyBlock(context.Context, *BlockUpdate) (*EmptyResponse, error)
+	NotifySlowdown(context.Context, *SlowdownAlert) (*EmptyResponse, error)
+	NotifyError(context.Context, *ErrorAlert) (*EmptyResponse, error)
 }
 
 // UnimplementedMonitorServer can be embedded to have forward compatible implementations.
@@ -317,6 +560,12 @@ func (*UnimplementedMonitorServer) Bye(ctx context.Context, req *EmptyRequest) (
 }
 func (*UnimplementedMonitorServer) NotifyBlock(ctx context.Context, req *BlockUpdate) (*EmptyResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method NotifyBlock not implemented")
+}
+func (*UnimplementedMonitorServer) NotifySlowdown(ctx context.Context, req *SlowdownAlert) (*EmptyResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method NotifySlowdown not implemented")
+}
+func (*UnimplementedMonitorServer) NotifyError(ctx context.Context, req *ErrorAlert) (*EmptyResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method NotifyError not implemented")
 }
 
 func RegisterMonitorServer(s *grpc.Server, srv MonitorServer) {
@@ -377,6 +626,42 @@ func _Monitor_NotifyBlock_Handler(srv interface{}, ctx context.Context, dec func
 	return interceptor(ctx, in, info, handler)
 }
 
+func _Monitor_NotifySlowdown_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(SlowdownAlert)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(MonitorServer).NotifySlowdown(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/monitor.Monitor/NotifySlowdown",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(MonitorServer).NotifySlowdown(ctx, req.(*SlowdownAlert))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _Monitor_NotifyError_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(ErrorAlert)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(MonitorServer).NotifyError(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/monitor.Monitor/NotifyError",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(MonitorServer).NotifyError(ctx, req.(*ErrorAlert))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 var _Monitor_serviceDesc = grpc.ServiceDesc{
 	ServiceName: "monitor.Monitor",
 	HandlerType: (*MonitorServer)(nil),
@@ -392,6 +677,14 @@ var _Monitor_serviceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "NotifyBlock",
 			Handler:    _Monitor_NotifyBlock_Handler,
+		},
+		{
+			MethodName: "NotifySlowdown",
+			Handler:    _Monitor_NotifySlowdown_Handler,
+		},
+		{
+			MethodName: "NotifyError",
+			Handler:    _Monitor_NotifyError_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
