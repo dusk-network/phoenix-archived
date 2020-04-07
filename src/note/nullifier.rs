9241875591a4c3c3f4d7b859a4c5bb9022c3ccb9
Jules de Smit <julesdesmit@gmail.com>
2020-04-07 10:50:13 +0200

    Implement conversion between normal nullifier and gRPC nullifier

--- src/note/nullifier.rs
+++ src/note/nullifier.rs
@@ -1,4 +1,4 @@
-use crate::BlsScalar;
+use crate::{rpc, utils, BlsScalar};
 
 #[derive(Debug, Default, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
 pub struct Nullifier(pub BlsScalar);
@@ -18,5 +18,12 @@ impl Into<BlsScalar> for Nullifier {
 impl AsRef<[u8]> for Nullifier {
     fn as_ref(&self) -> &[u8] {
         unimplemented!()
+    }
+}
+
+impl From<rpc::Nullifier> for Nullifier {
+    fn from(n: rpc::Nullifier) -> Self {
+        let scalar = utils::deserialize_bls_scalar(n.h.unwrap().data.as_slice()).unwrap();
+        Nullifier(scalar)
     }
 }
