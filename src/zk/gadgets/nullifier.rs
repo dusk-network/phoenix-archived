801e3cf51e68c59c86bdda7ef171a02eb3767c1e
Jules de Smit <julesdesmit@gmail.com>
2020-06-09 14:05:25 +0200

    Refactor nullifier gadget, and add a test
    Currently, the test is failing due to an unknown reason.

--- src/zk/gadgets/nullifier.rs
+++ src/zk/gadgets/nullifier.rs
@@ -1,17 +1,9 @@
 use crate::{zk, BlsScalar};
 
-use hades252::strategies::GadgetStrategy;
-use hades252::strategies::Strategy;
+use poseidon252::sponge::sponge::sponge_hash_gadget;
 
 /// Validate the input nullifiers
-pub fn nullifier<'a, P>(
-    mut composer: zk::Composer,
-    tx: &zk::ZkTransaction,
-    mut pi: P,
-) -> (zk::Composer, P)
-where
-    P: Iterator<Item = &'a mut BlsScalar>,
-{
+pub fn nullifier(mut composer: zk::Composer, tx: &zk::ZkTransaction) -> zk::Composer {
     let zero = *tx.zero();
     let one = *tx.one();
     let two = *tx.two();
@@ -25,10 +17,8 @@ where
         let mut sk_r_prime = *tx.zero();
 
         item.sk_r().iter().fold(one, |mut acc, bit| {
-            pi.next().map(|p| *p = BlsScalar::zero());
             composer.bool_gate(*bit);
 
-            pi.next().map(|p| *p = BlsScalar::zero());
             acc = composer.mul(
                 BlsScalar::one(),
                 acc,
@@ -39,7 +29,6 @@ where
 
             // TODO - The next two gates can be reduced to one with the ability to evaluate the
             // current sk_r_prime for the output of a poly_gate
-            pi.next().map(|p| *p = BlsScalar::zero());
             let dif = composer.mul(
                 BlsScalar::one(),
                 acc,
@@ -48,7 +37,6 @@ where
                 BlsScalar::zero(),
             );
 
-            pi.next().map(|p| *p = BlsScalar::zero());
             sk_r_prime = composer.add(
                 (BlsScalar::one(), sk_r_prime),
                 (BlsScalar::one(), dif),
@@ -62,28 +50,82 @@ where
         perm.copy_from_slice(&zero_perm);
         perm[1] = sk_r_prime;
         perm[2] = *item.idx();
-        let mut strat = GadgetStrategy::new(&mut composer);
-        strat.perm(&mut perm);
+        let output = sponge_hash_gadget(&mut composer, &perm);
 
-        pi.next().map(|p| *p = *item.nullifier());
         composer.add_gate(
-            perm[1],
+            output,
             zero,
             zero,
             -BlsScalar::one(),
             BlsScalar::one(),
             BlsScalar::one(),
             BlsScalar::zero(),
-            *item.nullifier(),
+            BlsScalar::zero(),
+            //*item.nullifier(),
         );
     }
 
-    (composer, pi)
+    composer
 }
 
 #[cfg(test)]
 mod tests {
+    use super::*;
     use crate::{crypto, utils, zk, Note, NoteGenerator, SecretKey, Transaction, TransparentNote};
+
+    #[test]
+    fn nullifier_gadget() {
+        zk::init();
+
+        let mut tx = Transaction::default();
+
+        let sk = SecretKey::default();
+        let pk = sk.public_key();
+        let value = 100;
+        let note = TransparentNote::output(&pk, value).0;
+        let merkle_opening = crypto::MerkleProof::mock(note.hash());
+        tx.push_input(note.to_transaction_input(merkle_opening, sk))
+            .unwrap();
+
+        let sk = SecretKey::default();
+        let pk = sk.public_key();
+        let value = 95;
+        let (note, blinding_factor) = TransparentNote::output(&pk, value);
+        tx.push_output(note.to_transaction_output(value, blinding_factor, pk))
+            .unwrap();
+
+        let sk = SecretKey::default();
+        let pk = sk.public_key();
+        let value = 2;
+        let (note, blinding_factor) = TransparentNote::output(&pk, value);
+        tx.push_output(note.to_transaction_output(value, blinding_factor, pk))
+            .unwrap();
+
+        let sk = SecretKey::default();
+        let pk = sk.public_key();
+        let value = 3;
+        let (note, blinding_factor) = TransparentNote::output(&pk, value);
+        tx.set_fee(note.to_transaction_output(value, blinding_factor, pk));
+
+        let mut composer = zk::Composer::new();
+
+        let zk_tx = zk::ZkTransaction::from_tx(&mut composer, &tx);
+
+        let mut composer = nullifier(composer, &zk_tx);
+        let mut transcript = zk::TRANSCRIPT.clone();
+
+        composer.add_dummy_constraints();
+        let circuit = composer.preprocess(&zk::CK, &mut transcript, &zk::DOMAIN);
+
+        let proof = composer.prove(&zk::CK, &circuit, &mut transcript);
+
+        assert!(proof.verify(
+            &circuit,
+            &mut transcript,
+            &zk::VK,
+            &composer.public_inputs()
+        ));
+    }
 
     #[test]
     fn tx_input_nullifier_invalid() {
