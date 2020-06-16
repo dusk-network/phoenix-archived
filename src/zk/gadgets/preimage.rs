94c7e8244688ded286aac5972b536502bff9cee9
CPerezz <carlos@dusk.network>
2020-06-16 22:05:10 +0200

    Fix preimage gadget test impl
    For some reason, use `composer.public_inputs` with `trace` and
    `trace-print` features was returning a Public Input vector which was
    smaller just by one element in comparaison to the circuit size.
    
    Since we don't use any Public Inputs, seting the vec as zeroes solves
    the problem.
    
    Anyway, we should consider to remove a lot of stuff from the
    zk::mod.rs file TBH.

--- src/zk/gadgets/preimage.rs
+++ src/zk/gadgets/preimage.rs
@@ -48,8 +48,6 @@ mod tests {
 
     #[test]
     fn preimage_gadget() {
-        zk::init();
-
         let mut tx = Transaction::default();
 
         let sk = SecretKey::default();
@@ -74,25 +72,29 @@ mod tests {
         tx.push_output(note.to_transaction_output(value, blinding_factor, pk))
             .unwrap();
 
-        let mut composer = zk::Composer::new();
-
+        let mut composer = dusk_plonk::constraint_system::StandardComposer::new();
         let zk_tx = zk::ZkTransaction::from_tx(&mut composer, &tx);
 
         preimage(&mut composer, &zk_tx);
         composer.add_dummy_constraints();
+        use dusk_plonk::commitment_scheme::kzg10::PublicParameters;
+        use dusk_plonk::fft::EvaluationDomain;
+        use merlin::Transcript;
 
-        let mut transcript = zk::TRANSCRIPT.clone();
-        let circuit = composer.preprocess(&zk::CK, &mut transcript, &zk::DOMAIN);
+        // Generate Composer & Public Parameters
+        let pub_params = PublicParameters::setup(1 << 17, &mut rand::thread_rng()).unwrap();
+        let (ck, vk) = pub_params.trim(1 << 16).unwrap();
+        let mut transcript = Transcript::new(b"TEST");
 
         composer.check_circuit_satisfied();
-
-        let proof = composer.prove(&zk::CK, &circuit, &mut transcript);
-
-        assert!(proof.verify(
-            &circuit,
+        let circuit = composer.preprocess(
+            &ck,
             &mut transcript,
-            &zk::VK,
-            &composer.public_inputs()
-        ));
+            &EvaluationDomain::new(composer.circuit_size()).unwrap(),
+        );
+
+        let proof = composer.prove(&ck, &circuit, &mut transcript.clone());
+
+        assert!(proof.verify(&circuit, &mut transcript, &vk, &[BlsScalar::zero()]));
     }
 }
