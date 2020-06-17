0056962139e20282739deb739208ff8e1dda0ed6
Kristoffer Ström <kristoffer@rymdkoloni.se>
2020-06-17 19:42:08 +0200

    Take Db by reference, not by Path

--- src/transaction/mod.rs
+++ src/transaction/mod.rs
@@ -1,15 +1,15 @@
 use crate::{
-    crypto, rpc, utils, zk, BlsScalar, Error, Note, NoteGenerator, ObfuscatedNote, PublicKey,
+    crypto, db, rpc, utils, zk, BlsScalar, Error, Note, NoteGenerator, ObfuscatedNote, PublicKey,
     SecretKey, TransparentNote,
 };
 
 use std::convert::TryFrom;
 use std::io::{self, Read, Write};
 use std::mem;
-use std::path::Path;
 use std::{fmt, ptr};
 
 use dusk_plonk::proof_system::Proof;
+use kelvin::ByteHash;
 
 use rand::distributions::{Distribution, Standard};
 use rand::Rng;
@@ -511,8 +511,8 @@ impl Transaction {
     /// Create a new transaction from a set of inputs/outputs defined by a rpc source.
     ///
     /// Will prove and verify the created transaction.
-    pub fn try_from_rpc_io<P: AsRef<Path>>(
-        db_path: P,
+    pub fn try_from_rpc_io<H: ByteHash>(
+        db: &db::Db<H>,
         fee_value: u64,
         inputs: &[rpc::TransactionInput],
         outputs: &[rpc::TransactionOutput],
@@ -522,7 +522,7 @@ impl Transaction {
         inputs
             .iter()
             .map(|i| {
-                TransactionInput::try_from_rpc_transaction_input(db_path.as_ref(), i.clone())
+                TransactionInput::try_from_rpc_transaction_input(db, i.clone())
                     .and_then(|i| transaction.push_input(i))
             })
             .collect::<Result<_, _>>()?;
@@ -546,8 +546,8 @@ impl Transaction {
     }
 
     /// Attempt to create a transaction from a rpc request.
-    pub fn try_from_rpc_transaction_db<P: AsRef<Path> + Clone>(
-        db_path: P,
+    pub fn try_from_rpc_transaction_db<H: ByteHash>(
+        db: &crate::db::Db<H>,
         tx: rpc::Transaction,
     ) -> Result<Self, Error> {
         let mut transaction = Transaction::default();
@@ -559,7 +559,7 @@ impl Transaction {
         tx.inputs
             .into_iter()
             .map(|i| {
-                TransactionInput::try_from_rpc_transaction_input(db_path.clone(), i)
+                TransactionInput::try_from_rpc_transaction_input(&db, i)
                     .and_then(|i| transaction.push_input(i))
             })
             .collect::<Result<_, _>>()?;
