d2e1e9a7173875e519bc565d9390a37c72656e43
Jules de Smit <julesdesmit@gmail.com>
2020-06-22 12:37:42 +0200

    Remove secret key gadget
    It will simply be a one-line wrapper for the pending `ecc_gate`
    function on PLONK, so it makes no sense to keep, as it doesnt add
    anything useful.

--- src/zk/gadgets/mod.rs
+++ src/zk/gadgets/mod.rs
@@ -4,7 +4,6 @@ pub use merkle::merkle;
 pub use nullifier::nullifier;
 pub use preimage::input_preimage;
 pub use range::range;
-pub use sk_r::sk_r;
 
 mod balance;
 mod commitment;
@@ -12,4 +11,3 @@ mod merkle;
 mod nullifier;
 mod preimage;
 mod range;
-mod sk_r;
