7ac576d723443879712764d45e4aeeca9f47cc5d
Jules de Smit <julesdesmit@gmail.com>
2020-06-22 11:35:42 +0200

    Remove obsolete test, add documentation for range gadget
    The test is not necessary, as the function simply wraps
    around a composer method.

--- src/zk/gadgets/range.rs
+++ src/zk/gadgets/range.rs
@@ -2,46 +2,10 @@ use crate::{BlsScalar, TransactionItem};
 
 use dusk_plonk::constraint_system::StandardComposer;
 
+/// This gadget simply wraps around the composer's `range_gate` function,
+/// but takes in any type that implements the [`TransactionItem`] trait,
+/// for ease-of-use in circuit construction.
 pub fn range<T: TransactionItem>(composer: &mut StandardComposer, item: &T) {
     let value = composer.add_input(BlsScalar::from(item.value()));
     composer.range_gate(value, 64);
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use crate::{crypto, Note, NoteGenerator, SecretKey, TransparentNote};
-
-    #[test]
-    fn range_gadget() {
-        let sk = SecretKey::default();
-        let pk = sk.public_key();
-        let value = 100;
-        let note = TransparentNote::output(&pk, value).0;
-        let merkle_opening = crypto::MerkleProof::mock(note.hash());
-        let input = note.to_transaction_input(merkle_opening, sk);
-
-        let mut composer = StandardComposer::new();
-
-        range(&mut composer, &input);
-        composer.add_dummy_constraints();
-        use dusk_plonk::commitment_scheme::kzg10::PublicParameters;
-        use dusk_plonk::fft::EvaluationDomain;
-        use merlin::Transcript;
-
-        // Generate Composer & Public Parameters
-        let pub_params = PublicParameters::setup(1 << 17, &mut rand::thread_rng()).unwrap();
-        let (ck, vk) = pub_params.trim(1 << 16).unwrap();
-        let mut transcript = Transcript::new(b"TEST");
-
-        let circuit = composer.preprocess(
-            &ck,
-            &mut transcript,
-            &EvaluationDomain::new(composer.circuit_size()).unwrap(),
-        );
-
-        let proof = composer.prove(&ck, &circuit, &mut transcript.clone());
-
-        assert!(proof.verify(&circuit, &mut transcript, &vk, &composer.public_inputs()));
-    }
 }
