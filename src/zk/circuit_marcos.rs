3fcba7783e317e79172e4babc604212662eec827
Luke Pearson <luke@dusk.network>
2020-07-07 12:50:39 +0200

    Add circuit macro
    To solve the issue of having set or
    hardcoded inputs and outputs of a
    DUSK token contract. This commit
    adds a macro which iterates through
    a given number of inputs and outputs
    for a transaction so they can be set
    correctly. There is a also a crossover
    option added.

--- src/zk/circuit_marcos.rs
+++ src/zk/circuit_marcos.rs
@@ -1,2 +1,58 @@
 /// This file will contain an implementation of the 
 /// different macros to vary the different circuit types
+
+use dusk_plonk::constraint_system::StandardComposer;
+use phoenix::{
+    utils, zk, Transaction}
+use crate::{NoteVariant, TransactionItem, ViewKey};
+
+macro_rules! phoenix_tx(inputs, outputs, crossover) {
+    () => { 
+        let mut tx = Transaction::default();
+
+        for i in 0..inputs {
+        let sk = SecretKey::default();
+        let vk = sk.view_key();
+        let pk = sk.public_key();
+        let value = 100;
+        let note = TransparentNote::output(&pk, value).0;
+        let merkle_opening = crypto::MerkleProof::mock(note.hash());
+        let input = note.to_transaction_input(merkle_opening, sk).unwrap();
+        tx.push_input(input);
+        }
+    
+        for i in 0..outputs {
+        let sk = SecretKey::default();
+        let vk = sk.view_key();
+        let pk = sk.public_key();
+        let value = 100;
+        let (note, blinding_factor) = TransparentNote::output(&pk, value);
+        let output = note.to_transaction_output(value, blinding_factor, pk))
+    
+        tx.push_output(output).unwrap();
+        }
+
+        for i in 0..outputs {
+        let sk = SecretKey::default();
+        let vk = sk.view_key();
+        let pk = sk.public_key();
+        let value = 100;
+        let (note, blinding_factor) = TransparentNote::output(&pk, value);
+        let output = note.to_transaction_output(value, blinding_factor, pk))
+        
+        tx.set_crossover;
+        }
+
+
+
+
+
+
+       
+        
+    };
+}
+
+fn send_to_obfuscate_variant! () {
+    
+}
